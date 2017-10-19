04da085a4f166ee0a2a34cf924753df47ead9fcb
David Hamilton <nova706@gmail.com>
2017-10-18 22:21:47 -0600

    Fix socket.io address

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ angular.module('AI').constant('debugMode', 0);
 angular.module('AI').constant('appName', 'Bishop AI');
 
 angular.module('AI').factory('socket', ['socketFactory', function (socketFactory) {
-    var socket = io("davidpaulhamilton.net", {path: 'http://davidpaulhamilton.net/bishop-ai/socket.io'});
+    var socket = io.connect("http://davidpaulhamilton.net", {path: '/bishop-ai/socket.io'});
 
     return socketFactory({
         ioSocket: socket
